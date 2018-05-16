a8261b718ffcccf54c05611c6090a25947327905
thaherster <thaherster@gmail.com>
2018-05-16 23:56:03 +0530

    upload --

--- routes/upload.js
+++ routes/upload.js
@@ -1,59 +1,65 @@
 var express = require('express');
 var router = express.Router();
+var path = require("path");
 var firebase = require('firebase/app'); require('firebase/auth'); require('firebase/storage'); require('firebase/database');
 var multer  = require('multer');
+var storageRef = firebase.storage().ref();
+var dbRef = firebase.database().ref();
+// const googleStorage = require('@google-cloud/storage');
 
 var storage = multer.diskStorage({
     destination: function(req, file, cb){
-    cb(null, 'public/apks');
-},
-filename:function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now()+'.apk');
+        cb(null, 'public/apks');
+    },
+    filename:function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now()+'.apk');
 
-}
+    }
 });
-var upload = multer({storage: storage});var storageRef = firebase.storage().ref();
-var dbRef = firebase.database().ref();
+
+var upload = multer({storage: storage});
+
 
 
 
 router.post('/', isAuthenticated,upload.single('apkfile'),function(req, res, next) {
-
     var user = firebase.auth().currentUser;
     var pushkey = dbRef.child('Applications/'+user.uid).push().key;
+
     console.log("LGINCLD Login Called"+user.uid+" "+pushkey);
     var appInfo = req.body;
-    const apkfile = req.file;
     console.log('LGINCLD form data', appInfo);
-    console.log('LGINCLD form apkfile', './public/apks/' + req.file.filename);
     var fs = require('fs');
     const testFolder = '/public/apks/';
-    var stream = fs.readFileSync(__dirname + '/..'+testFolder + req.file.filename);
-    var uploadTask = storageRef.child('APK/'+user.uid+'/'+pushkey+'.apk').put(stream);
+    console.log('LGINCLD form XXXX apkfile',path.join(__dirname, './..', testFolder + req.file.filename));
+    //
+    var stream = fs.readFileSync(path.join(__dirname, './..', testFolder + req.file.filename));
 
-    uploadTask.on('state_changed', function(snapshot){
 
-        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
-        switch (snapshot.state) {
-            case firebase.storage.TaskState.PAUSED: // or 'paused'
-                console.log('Upload is paused');
-                break;
-            case firebase.storage.TaskState.RUNNING: // or 'running'
-                console.log('Upload is running');
-                break;
-        }
-    }, function(error) {
-        console.log('Upload '+error.message);
+    console.log("blaaah aa " +storageRef.child('/APK/'+user.uid+'/filelist/'+pushkey+'.apk').put(stream));
+    //     .then(function(snapshot) {
+    //     console.log('Uploaded', snapshot.totalBytes, 'bytes.');
+    //     console.log('File metadata:', snapshot.metadata);
+    //     // Let's get a download URL for the file.
+    //     snapshot.ref.getDownloadURL().then(function(url) {
+    //         console.log('File available at', url);
+    //         // [START_EXCLUDE]
+    //         // [END_EXCLUDE]
+    //     });
+    // }).catch(function(error) {
+    //     // [START onfailure]
+    //     console.error('Upload failed:', error);
+    //     // [END onfailure]
+    // });
+    //
+    // dbRef.child('users/' + user.uid).set(req.body);
 
-        // Handle unsuccessful uploads
-    }, function() {
-        // Handle successful uploads on complete
-        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-        uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-            console.log('LGINCLD File available at', downloadURL);
-        });
-    });
+
+    // [END oncomplete]
+
+
+
+    console.log('END');
 
     // res.sendStatus(200);
 
