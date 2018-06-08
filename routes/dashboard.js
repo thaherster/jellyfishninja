b0a354e7c4ba191acddece2933dd87ac61fe6821
Thaher_bridge <thaher.m@bridge-india.in>
2018-06-08 18:31:05 +0530

    dasd

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 router.get('/', authenticationMiddleware,function(req, res, next) {
 
+    // database.getReference().child("Connected_Devices").child(req.user.uid).child("devices")   for device count
+    // var dbRef = firebase.database().ref().child('Applications/'+req.user.uid+'/projects/');   for app count
+    // var dbRef = firebase.database().ref().child('Reports/'+user+'/viaAndroid/').child(current_project); for report count and open bugs
+
     console.log("DASH "+req.user.uid);
     console.log("DASH "+req.user.email);
     console.log("DASH "+req.isAuthenticated());
