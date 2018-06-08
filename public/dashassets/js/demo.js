4e4e50f92c3bd14e41c2e49ecdb8290ad4135cee
Thaher_bridge <thaher.m@bridge-india.in>
2018-06-08 15:05:39 +0530

    dash

--- public/dashassets/js/demo.js
+++ public/dashassets/js/demo.js
@@ -22,7 +22,7 @@ demo = {
         dataDailySalesChart = {
             labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
             series: [
-                [12, 17, 7, 17, 23, 18, 38]
+                [0, 0, 0, 0, 0, 0, 0]
             ]
         };
 
@@ -52,7 +52,7 @@ demo = {
         dataDailySalesChart = {
             labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
             series: [
-                [12, 17, 7, 17, 23, 18, 38]
+                [0, 0, 0, 0, 0, 0, 0]
             ]
         };
 
@@ -81,7 +81,7 @@ demo = {
         dataCompletedTasksChart = {
             labels: ['12am', '3pm', '6pm', '9pm', '12pm', '3am', '6am', '9am'],
             series: [
-                [230, 750, 450, 300, 280, 240, 200, 190]
+                [0, 0, 0, 0, 0, 0, 0, 0]
             ]
         };
 
@@ -110,7 +110,7 @@ demo = {
         var dataEmailsSubscriptionChart = {
             labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
             series: [
-                [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
             ]
         };
