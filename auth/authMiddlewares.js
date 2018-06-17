92dea20b29c414869ea6bfe12aaa411f305fffc5
sreerajkrishnank <sreerajkrishnan.k@gmail.com>
2018-06-17 22:17:27 +0530

    bug fixing

--- auth/authMiddlewares.js
+++ auth/authMiddlewares.js
@@ -1,4 +1,4 @@
-const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
+const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn('/login');
 module.exports.authenticationMiddleware = function (req,res,next) {
     ensureLoggedIn(req,res,function () {
         res.locals.loggedIn = req.loggedIn = !!req.user && !!req.user.id;
@@ -6,4 +6,13 @@ module.exports.authenticationMiddleware = function (req,res,next) {
         res.locals.useremail = req.user.email
         next();
     })
+}
+
+module.exports.checkLoggedIn = function (req,res,next) {
+    res.locals.loggedIn = req.loggedIn = !!req.user && !!req.user.id;
+    if(req.loggedIn) {
+        res.locals.user = req.user.uuid;
+        res.locals.useremail = req.user.email
+    }
+    next();
 }
